be409cd8ed9751e2dd264ec192710f753f9aa65c
asus <asus@DESKTOP-H76TCGM>
2017-08-29 13:10:02 +0700

    Modul2Enkapsulasi

---
+++ src/latihan1/UjiBus.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author asus
+ */
+public class UjiBus {
+    public static void main(String[] args){
+        //membuat objek busMini dari kelas bus
+        Bus busMini = new Bus();
+        //memasukkan nilai jumlah penumpang dan penumpang maksimal ke dalam objek
+        busMini.penumpang = 5;
+        busMini.maxPenumpang = 15;
+        //memanggil method cetal pada kelas bus
+        busMini.cetak();
+        
+        //menambahkan penumpang pada busMini
+        busMini.penumpang = busMini.penumpang + 5;
+        //memanggil method cetak pada kelas bus
+        busMini.cetak();
+        
+        //mengurangi jumlah penumpang pada busMini
+        busMini.penumpang = busMini.penumpang - 2;
+        busMini.cetak();
+        
+        //menambah jumlah penumpang pada busMini
+        busMini.penumpang = busMini.penumpang + 8;
+        busMini.cetak();
+    }
+}
